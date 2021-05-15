b7549acc077f37594f2fa80deb14531260f68152
nikita0607 <ecfed203@gmail.com>
2021-05-15 23:55:21 +0800

    Создание DEBUG режима

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ try:
     from bs4 import BeautifulStoneSoup as bs
 except ImportError:
     try:
-        print("Загрузка библиоткеки beautifulsoup4...")
-        subprocess.run(["pip", "install", "beautifulsoup"])
+        print("Загрузка библиотеки beautifulsoup4...")
+        subprocess.run(["pip", "install", "beautifulsoup"], shell=True)
         from bs4 import BeautifulStoneSoup as bs
     except ImportError:
         print(f"Ошибка при импортировании библиотеки bs4")
     except Exception as ex:
         print(f"Неизвестная ошибка: {ex}")
-        exit()
+        exit(-1)
 
 
 class Searcher:
 
     def __init__(self):
         self.headers = {}
+        self.debug = 0
 
     def get_login_password(self):
         self.headers['login'] = input("Введите логин: ")
@@ -36,12 +37,16 @@ class Searcher:
         if url is None:
             url = input("Вставте ссылку: ")
 
-        req.get(url, self.headers)
+        _r = req.get(url, self.headers)
+        print(_r.status_code)
 
     def get_all_questions(self):
         self.check_and_get_login_password()
 
         start_url = input("Вставте стартовую ссылку: ")
+
+    def set_debug(self, status: int = 0):
+        self.debug = status
 
 
 def start() -> None:
@@ -57,14 +62,23 @@ def start() -> None:
         except Exception as ex:
             print(ex)
             continue
+        print()
 
         if choice == 4:
             break
 
-        if choice == 3:
+        elif choice == 6:
+            if searcher.debug:
+                print("DEBUG OFF")
+                searcher.set_debug(0)
+            else:
+                print("DEBUG ON")
+                searcher.set_debug(1)
+
+        elif choice == 3:
             searcher.get_login_password()
 
-        if choice == 1:
+        elif choice == 1:
             searcher.get_all_questions()
 
 
